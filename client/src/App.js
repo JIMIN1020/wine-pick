e4fbab4ab1f655cfa96ed26cfdbf041c25e3d495
JIMIN1020 <jimins4920@gmail.com>
2023-08-15 15:45:33 +0900

    #1 OpenAI API 연결

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,49 @@
+import axios from "axios";
+import { useState } from "react";
+
 function App() {
+  const [text, setText] = useState("");
+  const [messages, setMessages] = useState("");
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onSendClick = async () => {
+    // 새로운 message 만들기
+    const newMessage = [
+      {
+        role: "system",
+        content:
+          "You are a wine expert who has a detailed and deep knowledge of wine.",
+      },
+      {
+        role: "user",
+        content: text,
+      },
+    ];
+    // reset
+    setText("");
+
+    // 서버에 요청 보내기
+    axios
+      .post("/chat", newMessage)
+      .then((res) => {
+        setMessages(res.data);
+      })
+      .catch((err) => {
+        console.log("Error response:", err.response);
+      });
+  };
+
   return (
     <div>
       <h1>Wine bot</h1>
-      <input type="text" />
-      <button>send</button>
+      <input type="text" value={text} onChange={onChange} />
+      <button onClick={onSendClick}>send</button>
+      <br />
       <span></span>
+      <span>answer: {messages}</span>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,14 @@
-const express = require("express"); // express 모듈 불러오기
+// 필요한 모듈 불러오기
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express(); // app에 express 담기 -> 이를 통해 서버 관리!
+
+// body-parser 세팅
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 // port 할당: 0~65535 사이의 정수로된 임의의 숫자
 const PORT = process.env.PORT || 4000;
@@ -12,4 +21,29 @@ app.get("/", (req, res) => {
 // 서버 실행하기 -> listen
 app.listen(PORT, () => {
   console.log(`Server On : http://localhost:${PORT}/`);
+});
+
+/* ---------- OpenAI API ---------- */
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// endpoint for chat GPT
+app.post("/chat", async (req, res) => {
+  // 요청값 -> 메세지 받아오기
+  const messages = req.body;
+
+  // 요청 데이터
+  const chatCompletion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    temperature: 2,
+    messages: messages,
+  });
+
+  // response
+  res.send(chatCompletion.data.choices[0].message.content);
 });
