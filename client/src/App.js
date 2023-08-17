50c390f68e82372cd1fc862da291fcade16e58ee
JIMIN1020 <jimins4920@gmail.com>
2023-08-17 21:22:41 +0900

    #5 검색 결과 렌더링 작업

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import GlobalStyle from "./GlobalStyle";
 import { styled } from "styled-components";
 import MessageBox from "./component/MessageBox";
 import InputForm from "./component/InputForm";
+import WineInfo from "./component/WineInfo";
 
 function App() {
   const [response, setResponse] = useState(""); // GPT 응답
   const [keywords, setKeywords] = useState([]); // GPT 답변 가공한 검색 키워드들
   const [wineData, setWineData] = useState([]); // 네이버에게 전달받은 와인 데이터
+  const [loading, setLoading] = useState(false); // 로딩중 사인
 
   /* ------------- GPT 답변 가공하기 ------------- */
   useEffect(() => {
@@ -30,6 +32,7 @@ function App() {
           .then((res) => setWineData((prev) => [...prev, res.data.items[0]]))
           .catch((err) => console.log("error!: ", err));
       });
+      setLoading(false);
     };
     // 키워드 5개가 존재하면 검색 시작!
     if (keywords.length === 5) {
@@ -39,6 +42,7 @@ function App() {
 
   /* ------------- OpenAI API 요청 ------------- */
   const onSendClick = async (msg) => {
+    setLoading(true);
     // 메세지 만들기
     const question = `please recommend 5 ${msg.type} wine products which is ${msg.body} bodied, ${msg.tannin} tannin, ${msg.sweetness} sweetness, ${msg.acidity} acidity. Answer specific wine names only without numbering.`;
 
@@ -89,7 +93,22 @@ function App() {
         <MessageBox />
         <InputForm onSendClick={onSendClick} />
       </FormBox>
-      <div>{console.log(wineData)}</div>
+      {!loading && wineData.length > 0 ? (
+        <Temp>
+          <h2>내 취향에 맞는 와인은?</h2>
+        </Temp>
+      ) : undefined}
+      <WineBox>
+        {!loading && wineData.length > 0
+          ? wineData.map((wine, i) => {
+              if (wine) {
+                return <WineInfo wine={wine} id={i} />;
+              } else {
+                return null;
+              }
+            })
+          : undefined}
+      </WineBox>
     </Container>
   );
 }
@@ -101,12 +120,14 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  overflow: auto;
 `;
 
 const FormBox = styled.div`
   width: 95%;
   height: 600px;
   border-radius: 10px;
+  margin-bottom: 30px;
 
   display: flex;
 
@@ -114,4 +135,21 @@ const FormBox = styled.div`
   box-shadow: 0px 0px 2px 0px rgba(164, 164, 164, 1);
   -webkit-box-shadow: 0px 0px 2px 0px rgba(164, 164, 164, 1);
   -moz-box-shadow: 0px 0px 2px 0px rgba(164, 164, 164, 1);
+`;
+
+const Temp = styled.div`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  color: white;
+`;
+
+const WineBox = styled.div`
+  width: 95%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
---
+++ client/src/component/WineInfo.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { styled } from "styled-components";
+import wineImg from "../assets/images/wine-bottle.png";
+
+const WineInfo = ({ wine }) => {
+  return (
+    <Container>
+      <Thumbnail>
+        {wine.thumbnail ? (
+          <img src={wine.thumbnail} height="180" />
+        ) : (
+          <img src={wineImg} width="150" />
+        )}
+      </Thumbnail>
+      <Description>
+        <h3>{wine.title}</h3>
+        <p>{wine.description ? wine.description : "상세 설명이 없습니다."}</p>
+      </Description>
+    </Container>
+  );
+};
+
+export default WineInfo;
+
+const Container = styled.div`
+  width: 600px;
+  height: 250px;
+  background-color: white;
+  margin: 20px 0px;
+  border-radius: 15px;
+
+  display: flex;
+`;
+
+const Thumbnail = styled.div`
+  width: 170px;
+  height: 250px;
+  border-radius: 15px;
+  overflow: hidden;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Description = styled.div`
+  width: 380px;
+  height: 200px;
+  box-sizing: border-box;
+  padding: 15px 15px;
+
+  h3 {
+    font-size: 24px;
+  }
+
+  p {
+    line-height: 25px;
+  }
+`;
