5be4003f8b88904584d683db9fc8af86aab1b310
JIMIN1020 <jimins4920@gmail.com>
2023-11-05 21:31:13 +0900

    jsx로 변경, 새 페이지 생성

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Pairing from "./routes/Pairing";
+import Recommend from "./routes/Recommend";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/pairing" element={<Pairing />} />
+      <Route path="/recommend" element={<Recommend />} />
     </Routes>
   );
 };
---
+++ client/src/routes/Pairing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Pairing() {
+  return <div>Pairing</div>;
+}
+
+export default Pairing;
